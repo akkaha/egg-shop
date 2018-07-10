ea2f72e14743ef2dea62776115772c83f956561c
zhengshaodong <ngxcorpio@gmail.com>
2018-07-10 23:41:42 +0800

    save UserExt when create new order

---
+++ src/main/java/cc/akkaha/shop/controllers/model/UserExt.java
@@ -0,0 +1,24 @@
+package cc.akkaha.shop.controllers.model;
+
+import java.util.List;
+
+public class UserExt {
+    private List<String> sixWeights;
+    private List<String> sevenWeights;
+
+    public List<String> getSixWeights() {
+        return sixWeights;
+    }
+
+    public void setSixWeights(List<String> sixWeights) {
+        this.sixWeights = sixWeights;
+    }
+
+    public List<String> getSevenWeights() {
+        return sevenWeights;
+    }
+
+    public void setSevenWeights(List<String> sevenWeights) {
+        this.sevenWeights = sevenWeights;
+    }
+}
